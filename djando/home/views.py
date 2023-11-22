a359d6285f9efafa240eeffd5f78f4fd60c7d531
asifbhat <asifsultanbhat@gmail.com>
2023-11-23 00:35:42 +0530

    html made and edit in home views

--- djando/home/views.py
+++ djando/home/views.py
@@ -5,5 +5,23 @@ def index(request):
     }
     return render(request,'index.html',context)
     #return HttpResponse("This is homepage")
+def b.fashion(request):
+    return render(request,'BOY FASHION.html')
+def feeding(request):
+    return render(request,'FEEDING.html')
+def g.fashion(request):
+    return render(request,'GIRL FASHION.html')
+def toys(request):
+    return render(request,'TOYS.html')
+def footwear(request):
+    return render(request,'FOOTWEAR.html')
 def contact(request):
-    return HttpResponse("This contact page")
+    return render(request,'CONTACT US.html')
+def contact(request):
+    return render(request,'index.html')
+def contact(request):
+    return render(request,'index.html')
+def contact(request):
+    return render(request,'index.html')
+def contact(request):
+    return render(request,'index.html')
